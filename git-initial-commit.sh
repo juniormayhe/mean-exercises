4098e7407639da0aa7de887255a8826f5c641e32
Wanderley Mayhe Junior <juniormayhe@gmail.com>
2017-05-12 00:34:37 +0000

    Initial commit

--- git-initial-commit.sh
+++ git-initial-commit.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 PROJECT_NAME=mean-exercises
+URL=https://github.com/juniormayhe/mean-exercises.git
 cd /$PROJECT_NAME
 git init
 git add .
 git config --global user.email "juniormayhe@gmail.com"
 git config --global user.name "Wanderley Mayhe Junior"
 git commit -m "Initial commit"
+git remote add origin remote repository $URL
